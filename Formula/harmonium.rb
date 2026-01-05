5d4738492ed7762355284351b25e1716bf45fcf9
William Quintal <williamquintal95@gmail.com>
2026-01-04 23:33:31 -0500

    harmonium 0.0.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/harmonium.rb
+++ Formula/harmonium.rb
@@ -4,10 +4,10 @@
 class Harmonium < Formula
   desc "AI-powered procedural music generator CLI"
   homepage "https://github.com/bascanada/harmonium"
-  url "https://github.com/bascanada/harmonium/releases/download/v0.0.3/harmonium-cli-macos-universal.tar.gz"
-  sha256 "365a4ff22f9ac8871fbf673088507ca3112a7177f1082526345f944c68473409"
+  url "https://github.com/bascanada/harmonium/releases/download/v0.0.4/harmonium-cli-macos-universal.tar.gz"
+  sha256 "1b61c2d9748e31a884823a5b9243bf3a652a0aaf115d5bb5ecb9e40b5b5dce0f"
   license "MIT"
-  version "0.0.3"
+  version "0.0.4"
 
   def install
     bin.install "harmonium"
