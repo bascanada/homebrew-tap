8cd654c9e3bdbf47922ae7817b1bf0ebbe8b5b61
William Quintal <williamquintal95@gmail.com>
2026-01-05 00:19:16 -0500

    Update to v0.0.4 with correct URL (v prefix)

--- Casks/harmonium-plugins.rb
+++ Casks/harmonium-plugins.rb
@@ -1,8 +1,8 @@
 cask "harmonium-plugins" do
-  version "v0.0.4"
+  version "0.0.4"
   sha256 "e82c1f90b163fdb5dcb59eab04e3f03e23c2c096b8e78e252c71d7e822f18f8c"
 
-  url "https://github.com/bascanada/harmonium/releases/download/#{version}/harmonium-plugins-macos-universal.zip"
+  url "https://github.com/bascanada/harmonium/releases/download/v#{version}/harmonium-plugins-macos-universal.zip"
   name "Harmonium Plugins"
   desc "AI-powered generative music VST3/CLAP plugins"
   homepage "https://github.com/bascanada/harmonium"
